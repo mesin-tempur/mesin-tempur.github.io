2435264e1b96b86e769dfcd4c518c96bdabf06f7
Rama Ibrahim <ibrahim.rama@gmail.com>
2016-02-11 06:13:43 +0700

    category-list page

--- js/app.js
+++ js/app.js
@@ -16,11 +16,14 @@ function searchInputReveal() {
 searchInputReveal();
 
 function categoriMultilevel() {
-	var wrap 	= $('.cat-list'),
-		parent 	= wrap.find('.first-level-cat'),
-		child 	= wrap.find('.second-level-cat'),
-		goChild = wrap.find('.has-child'),
-		goParent = wrap.find('.back');
+	var wrap 		= $('.cat-list'),
+		parent 		= wrap.find('.first-level-cat'),
+		child 		= wrap.find('.second-level-cat'),
+		grandChild 	= wrap.find('.third-level-cat'),
+		goChild 	= wrap.find('.has-child'),
+		goParent 	= wrap.find('.back-to-first'),
+		goSecond	= wrap.find('.back-to-second'),
+		goChild3rd	= wrap.find('.second-level-cat-link');
 
 	goChild.on('click tap', function() {
 		parent.addClass('hidden');
@@ -29,6 +32,14 @@ function categoriMultilevel() {
 	goParent.on('click tap', function() {
 		parent.removeClass('hidden');
 		child.addClass('hidden');
+	});
+	// 3rd Level Category
+	goChild3rd.on('click tap', function() {
+		child.addClass('hidden');
+	});
+	goSecond.on('click tap', function() {
+		grandChild.addClass('hidden');
+		$($(this).attr('data-target')).removeClass('hidden');
 	});
 }
 categoriMultilevel();
